51907625797e0a9fb33635e6740a4c41a0b5c418
Sarah Williams <srwilliams8678@gmail.com>
2019-09-18 14:50:09 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,16 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
-    song.count
+    songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
-    artist.count
+    artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artist.all
+    artists.all
   end
 end
